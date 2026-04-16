98b133053ae8f3c8b72643508774698a8e3a468d
jesfuen <jesusdelafuente2002@gmail.com>
2026-04-16 12:58:35 +0200

    Añadir función create_nonce

--- authserver.c
+++ authserver.c
@@ -1,27 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <err.h>
 #include <limits.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <fcntl.h>
 
-struct client
+typedef struct
 {
     char login[256];
     // Resto de campos key, nonce, timestamp...
-};
+} client;
+
+typedef struct
+{
+    uint64_t high;
+    uint64_t low;
+} nonce;
 
 int
 to_int(char *str)
 {
     long port;
     char *endptr;
+    
 
     port = strtol(str,&endptr,10);
     if (port < 0 || port > INT_MAX || *endptr != '\0') {
         errx(EXIT_FAILURE, "error: bad value %s", str);
     }
-
 
     return (int)port;
 }
@@ -30,6 +38,29 @@ void
 usage()
 {
     errx(EXIT_FAILURE,"usage: ./authserver file [int]");
+}
+
+nonce
+create_nonce(int *cont) {
+    // Parte pseudoaleatoria de 64 bits que se lee de /dev/urandom
+    nonce n;
+    int urandom_fd;
+
+    urandom_fd = open("/dev/urandom",O_RDONLY);
+    if (urandom_fd < 0) {
+        err(EXIT_FAILURE,"open /dev/urandom failed");
+    }
+
+    if (read(urandom_fd,&n.high,sizeof(n.high)) != sizeof(n.high)) {
+        err(EXIT_FAILURE,"read /dev/urandom failed");
+    }
+
+    close(urandom_fd);
+    *cont++;
+    n.low = (uint64_t)*cont;
+
+    return n;
+
 }
 
 int
@@ -41,7 +72,6 @@ main(int argc, char *argv[])
     int port = 9999;
     int fd;
     int sockfd;
-    int cont;
 
     if (argc < 2) {
         usage();
